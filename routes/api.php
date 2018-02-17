9d470ac7aed780a3e6d2a56dbf03378ee9b1a60a
ADAM FIRDAUS <adam.firdauz22@gmail.com>
2018-02-17 16:12:38 +0700

    users/profile authorization

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('users', 'UserController@users');
 Route::post('auth/register', 'AuthController@register');
 Route::post('auth/login', 'AuthController@login');
-Route::get('users/profile', 'UserController@profile')->middleware('auth::api');
+Route::get('users/profile', 'UserController@profile')->middleware('auth:api');
